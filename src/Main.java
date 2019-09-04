a3781c1a824b8fddf68d70fc32ede50ad3404a93
JuanPabloH <jpherrera69@gmail.com>
2019-09-04 16:56:31 -0500

    Meses agregados para agendar

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor doc= new Doctor();
-        doc.name="JuanPi";
-        doc.showName();
-        doc.showId();
-
-        Doctor doc1= new Doctor();
-
-        doc1.showId();
 
         showMenu();
     }
